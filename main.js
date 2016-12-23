d95d8c54a285b1da6a3fcebf6ea31d96932a263c
knod <knodding@gmail.com>
2016-12-22 21:38:43 -0500

    Implements detect-lang, abstracts removing unwanted elements
    Note: detect-lang is used with a smaller sample than in previous
    versions. May be worth checking if detect-lang is lazy about
    how it guesses, or if it really does check all the text you
    give it.

--- main.js
+++ main.js
@@ -12,7 +12,8 @@
 
 (function(){
 
-	var unfluff = require('@knod/unfluff');
+	var unfluff = require('@knod/unfluff'),
+		detect 	= require('detect-lang');
 
 	var queue 		= new Queue(),
 		timer 		= new ReaderlyTimer(),
@@ -26,12 +27,36 @@
 	})
 
 
-	function read ( text ) {
+	var read = function ( text ) {
 		// TODO: If there's already a queue, start where we left off
 		queue.process( text );
 		timer.start( queue );
 		return true;
-	}
+	};
+
+
+	var cleanHTML = function ( $node ) {
+		$node.find('sup').remove();
+		$node.find('script').remove();
+		$node.find('style').remove();
+		return $node;
+	};
+
+
+	var smallSample = function ( $node ) {
+	// Get three sample paragraphs from around the middle of the page
+		var sample 	= '',
+			$ps 	= $node.find('p'),
+			numPs 	= $ps.length;
+		if ( $ps[0] ) {
+			var base = Math.floor(numPs/3);
+			sample += $($ps[base]).text();
+			sample += ' ' + $($ps[base * 2]).text();
+			sample += ' ' + $($ps[base * 3]).text();
+		}
+
+		return sample;
+	};
 
 
 	chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
@@ -41,19 +66,22 @@
 
 		var func = request.functiontoInvoke;
 		if ( func === "readSelectedText" ) {
+
 			read( request.selectedText );
 
 		} else if ( func === "readFullPage" ) {
 
-			var $clone = $('html').clone();
-			$clone.find('sup').remove();
-			$clone.find('script').remove();
-			$clone.find('style').remove();
+			var $clone = $('html').clone(),
+				$clean = cleanHTML( $clone );
 
-			var lang = 'en',
-				cmds = unfluff.lazy( $clone.html(), lang ),
-				data = cmds.text();
-			read( data );
+			var sampleText = smallSample( $clean );
+
+			detect( sampleText ).then(function (data) {
+				var lang = data.iso6391 || 'en',
+					cmds = unfluff.lazy( $clean.html(), lang ),
+					data = cmds.text();
+				read( data )
+			});
 
 		}  // end if event is ___
 
