1a6f265e1530911c28688d487dbecd5a759c4260
knod <knodding@gmail.com>
2016-12-22 21:02:02 -0500

    Adds rough implementation of @knod/unfluff

--- main.js
+++ main.js
@@ -7,9 +7,12 @@
 * - Cache reading progress?
 * - Trigger pause on clicking of central element, not
 * 	just text
+* - Add function "cleanHTML" to get rid of unwanted elements
 */
 
 (function(){
+
+	var unfluff = require('@knod/unfluff');
 
 	var queue 		= new Queue(),
 		timer 		= new ReaderlyTimer(),
@@ -23,7 +26,7 @@
 	})
 
 
-	function playReadContent ( text ) {
+	function read ( text ) {
 		// TODO: If there's already a queue, start where we left off
 		queue.process( text );
 		timer.start( queue );
@@ -38,35 +41,20 @@
 
 		var func = request.functiontoInvoke;
 		if ( func === "readSelectedText" ) {
-			playReadContent( request.selectedText );
+			read( request.selectedText );
 
 		} else if ( func === "readFullPage" ) {
 
-			var getArticle = $.get( 'https://readparser.herokuapp.com/?url=' + document.URL );
-			getArticle.success(function( result ) {
+			var $clone = $('html').clone();
+			$clone.find('sup').remove();
+			$clone.find('script').remove();
+			$clone.find('style').remove();
 
-				playReadContent( result );
+			var lang = 'en',
+				cmds = unfluff.lazy( $clone.html(), lang ),
+				data = cmds.text();
+			read( data );
 
-			}).error(function( jqXHR, textStatus, errorThrown ) {
-				var text = '';
-				var elements = $('p, li, h1, h2, h3, h4, h5, h6, span, pre');
-				elements.each(function(index, element) {
-					element = $(element);
-					var elementText = element
-						.clone()
-						.children('sup')
-						.remove()
-						.end()
-						.text()
-						.trim();
-					if (elementText.length >= 60)
-						if (!(element.tagName === 'LI' && elementText.includes('    ')))
-							text += " " + elementText;
-				});  // end for each desired element
-
-				playReadContent(text);
-
-			});  // end getArticle
 		}  // end if event is ___
 
 	});  // End event listener
